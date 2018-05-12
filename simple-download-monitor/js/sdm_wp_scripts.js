28ebdb5ad01626d37c84444ab7ebdae60d97f880
Evgeniy Nezhidaev <dareks84@gmail.com>
2018-05-12 13:49:56 +0300

    allow using multiple download buttons with terms&cond checkbox in one page

--- simple-download-monitor/js/sdm_wp_scripts.js
+++ simple-download-monitor/js/sdm_wp_scripts.js
@@ -1,9 +1,9 @@
 // Simple Download Monitor frontend scripts
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     // Populate all nested titles and links 
-    $('li.sdm_cat').each(function() {
+    $('li.sdm_cat').each(function () {
 
         var $this = $(this);
         this_slug = $this.attr('id');
@@ -17,17 +17,17 @@ jQuery(document).ready(function($) {
                     cat_slug: this_slug,
                     parent_id: this_id
                 },
-        function(response) {
+                function (response) {
 
-            // Loop array returned from ajax function
-            $.each(response.final_array, function(key, value) {
+                    // Loop array returned from ajax function
+                    $.each(response.final_array, function (key, value) {
 
-                // Populate each matched post title and permalink
-                $this.children('.sdm_placeholder').append('<a href="' + value['permalink'] + '"><span class="sdm_post_title" style="cursor:pointer;">' + value['title'] + '</span></a>');
-            });
+                        // Populate each matched post title and permalink
+                        $this.children('.sdm_placeholder').append('<a href="' + value['permalink'] + '"><span class="sdm_post_title" style="cursor:pointer;">' + value['title'] + '</span></a>');
+                    });
 
-            $this.children('span').append('<span style="margin-left:5px;" class="sdm_arrow">&#8616</span>');
-        }
+                    $this.children('span').append('<span style="margin-left:5px;" class="sdm_arrow">&#8616</span>');
+                }
         );
     });
 
@@ -35,7 +35,7 @@ jQuery(document).ready(function($) {
     $('li.sdm_cat').children('.sdm_placeholder').hide();
 
     // Slide toggle for each list item
-    $('body').on('click', '.sdm_cat_title', function(e) {
+    $('body').on('click', '.sdm_cat_title', function (e) {
 
         // If there is any html.. then we have more elements
         if ($(this).next().html() != '') {
@@ -52,34 +52,40 @@ jQuery(document).ready(function($) {
 
     // Check if terms checkbox is enabled.
     if ($('.sdm-termscond-checkbox').length) {
-        
+
         $.each($('.sdm-termscond-checkbox'), function () {
             if (!$(this).is(':checked')) {
                 var cur = $(this).children(':checkbox');
-                var btn = $(cur).closest('form').find('a.sdm_download');
+                var btn = $(cur).closest('form').find('a.sdm_download,a.sdm_download_with_condition');
                 $(btn).addClass('sdm_disabled_button');
             }
         });
-        
-        $('.sdm-download-form').on('submit', function () {
-            if ($('.agree_termscond').is(':checked')) {
-                $('.sdm-termscond-checkbox').removeClass('sdm_general_error_msg');
-                return true;
-            } else {
-                $('.sdm-termscond-checkbox').addClass('sdm_general_error_msg');
-            }
-            return false;
+
+        $.each($('.sdm-download-form'), function () {
+            var form = $(this);
+            form.on('submit', function () {
+                if ($('.agree_termscond', form).is(':checked')) {
+                    $('.sdm-termscond-checkbox', form).removeClass('sdm_general_error_msg');
+                    return true;
+                } else {
+                    $('.sdm-termscond-checkbox', form).addClass('sdm_general_error_msg');
+                }
+                return false;
+            });
         });
 
-        $('.agree_termscond').on('click', function () {
-             if ($(this).is(':checked')) {
-                 $('.sdm_download_with_condition').removeClass('sdm_disabled_button');
-                 $('.sdm-termscond-checkbox').removeClass('sdm_general_error_msg');
-             } else {
-                 $('.sdm_download_with_condition').addClass('sdm_disabled_button');
-                 $('.sdm-termscond-checkbox').addClass('sdm_general_error_msg');
-             }
+        $.each($('.agree_termscond'), function () {
+            var element = $(this);
+            var form = element.closest('form');
+            element.on('click', function () {
+                if (element.is(':checked')) {
+                    $('.sdm_download_with_condition', form).removeClass('sdm_disabled_button');
+                    $('.sdm-termscond-checkbox', form).removeClass('sdm_general_error_msg');
+                } else {
+                    $('.sdm_download_with_condition', form).addClass('sdm_disabled_button');
+                    $('.sdm-termscond-checkbox', form).addClass('sdm_general_error_msg');
+                }
+            });
         });
-
     }
 });
