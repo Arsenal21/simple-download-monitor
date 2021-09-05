77b602a92ca5928dcb4c72234beab65cb5d1531d
amin0 <amin0@DESKTOP-KUST9TJ.Home>
2021-09-05 11:07:12 +1000

    fixed link to documentation

--- simple-download-monitor/includes/admin-side/sdm-admin-edit-download.php
+++ simple-download-monitor/includes/admin-side/sdm-admin-edit-download.php
@@ -281,7 +281,7 @@ class SDM_Admin_Edit_Download {
 		echo "<input type='text' class='code' onfocus='this.select();' readonly='readonly' value='" . esc_attr( $shortcode_text ) . "' size='40'>";
 
 		echo '<br /><br />';
-		esc_html_e( 'Read the full shortcode usage documentation <a href="https://simple-download-monitor.com/miscellaneous-shortcodes-and-shortcode-parameters/" target="_blank">here</a>.', 'simple-download-monitor' );
+		_e( 'Read the full shortcode <a href="https://simple-download-monitor.com/miscellaneous-shortcodes-and-shortcode-parameters/" target="_blank">usage documentation here</a>.', 'simple-download-monitor' );
 	}
 
 	public function save_post_handler( $post_id, $post, $update ) {
