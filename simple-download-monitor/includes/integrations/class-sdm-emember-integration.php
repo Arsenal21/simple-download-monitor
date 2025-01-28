eaf9e78d8e19a4e583d7d140ccc13b181e3604b0
adilrabid <rabid1819@gmail.com>
2025-01-28 16:07:26 +0600

    download process request now runs on wp hook

--- simple-download-monitor/includes/integrations/class-sdm-emember-integration.php
+++ simple-download-monitor/includes/integrations/class-sdm-emember-integration.php
@@ -4,58 +4,56 @@ class SDM_eMember_Integration {
 
 	public $error_msg = '';
 
-	public function __construct()
-	{
-		if (! function_exists('is_plugin_active')) {
-			include_once(ABSPATH . 'wp-admin/includes/plugin.php');
+	public function __construct() {
+		if ( ! function_exists( 'is_plugin_active' ) ) {
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 		}
 
-		if (is_plugin_active('wp-eMember/wp_eMember.php')) {
-			add_action('plugin_loaded', array($this, 'setup_integration'));
+		if ( is_plugin_active( 'wp-eMember/wp_eMember.php' ) ) {
+			add_action( 'plugins_loaded', array( $this, 'setup_eMember_integration' ) );
+			add_action( 'sdm_file_protection_settings_updated', array( $this, 'update_eMember_integration_settings' ) );
+			add_action( 'sdm_after_file_protection_settings_fields', array(
+				$this,
+				'show_eMember_integration_settings'
+			) );
 		}
 	}
 
-	public function setup_integration()
-	{
-		add_action('sdm_file_protection_settings_updated', array($this, 'update_integration_settings'));
-		add_action('sdm_after_file_protection_settings_fields', array($this, 'show_integration_settings'));
-
-		$settings = get_option('sdm_global_options', array());
+	public function setup_eMember_integration() {
+		$settings = get_option( 'sdm_global_options', array() );
 
 		// Check if SWPM access control feature enabled for SDM downloads.
-		$is_swpm_access_control_enabled = isset($settings['enable_eMember_access_control']) && ! empty($settings['enable_eMember_access_control']) ? true : false;
-		if ($is_swpm_access_control_enabled) {
+		$is_swpm_access_control_enabled = isset( $settings['enable_eMember_access_control'] ) && ! empty( $settings['enable_eMember_access_control'] ) ? true : false;
+		if ( $is_swpm_access_control_enabled ) {
 			// Block download process request for unauthorized visitors.
-			add_action( 'sdm_process_download_request', array( $this, 'check_download_process' ), 10, 2 );
-			add_action( 'sdm_sf_process_download_request', array( $this, 'check_download_process' ), 10, 2 );
+			add_action( 'sdm_process_download_request', array( $this, 'check_download_process' ), 5, 2 );
+			add_action( 'sdm_sf_process_download_request', array( $this, 'check_download_process' ), 5, 2 );
 
 			// Override download button html for unauthorized visitor.
-			add_filter('sdm_download_button_code_html', array($this, 'disable_sdm_download_button'));
+			add_filter( 'sdm_download_button_code_html', array( $this, 'disable_sdm_download_button' ) );
 		}
 	}
 
-	public function update_integration_settings()
-	{
-		$settings = get_option('sdm_global_options', array());
+	public function update_eMember_integration_settings() {
+		$settings = get_option( 'sdm_global_options', array() );
 
 		// Prepare settings array to save.
-		$settings['enable_eMember_access_control'] = isset($_POST['enable_eMember_access_control']) ? 'checked="checked"' : '';
+		$settings['enable_eMember_access_control'] = isset( $_POST['enable_eMember_access_control'] ) ? 'checked="checked"' : '';
 
 		// Save the settings.
-		update_option('sdm_global_options', $settings);
+		update_option( 'sdm_global_options', $settings );
 	}
 
-	public function show_integration_settings()
-	{
-		$settings = get_option('sdm_global_options', array());
+	public function show_eMember_integration_settings() {
+		$settings = get_option( 'sdm_global_options', array() );
 
-		$enable_eMember_access_control = isset($settings['enable_eMember_access_control']) && ! empty($settings['enable_eMember_access_control']) ? $settings['enable_eMember_access_control'] : '';
+		$enable_eMember_access_control = isset( $settings['enable_eMember_access_control'] ) && ! empty( $settings['enable_eMember_access_control'] ) ? $settings['enable_eMember_access_control'] : '';
 
 		$output = '<tr>';
-		$output .= '<th scope="row">' . __('Enable eMember Access Control', 'simple-download-monitor') . '</th>';
+		$output .= '<th scope="row">' . __( 'Enable eMember Access Control', 'simple-download-monitor' ) . '</th>';
 		$output .= '<td>';
-		$output .= '<input name="enable_eMember_access_control" id="enable_eMember_access_control" type="checkbox" ' . esc_attr($enable_eMember_access_control) . '/>';
-		$output .= '<p class="description">' . __('Check this to enable WP eMember access control', 'simple-download-monitor') . '</p>';
+		$output .= '<input name="enable_eMember_access_control" id="enable_eMember_access_control" type="checkbox" ' . esc_attr( $enable_eMember_access_control ) . '/>';
+		$output .= '<p class="description">' . __( 'Check this to enable WP eMember access control', 'simple-download-monitor' ) . '</p>';
 		$output .= '</td>';
 		$output .= '</tr>';
 		echo $output;
@@ -88,6 +86,7 @@ class SDM_eMember_Integration {
 			} else {
 				$this->error_msg = str_replace( 'view this content', 'download this item', get_login_link() );
 			}
+
 			return false;
 		}
 
@@ -97,7 +96,7 @@ class SDM_eMember_Integration {
 	/**
 	 * Disables the download button if it is restricted for current visitor.
 	 */
-	public function disable_sdm_download_button($btn_html) {
+	public function disable_sdm_download_button( $btn_html ) {
 		$dom = new DOMDocument();
 		// Suppress warnings due to malformed HTML
 		libxml_use_internal_errors( true );
@@ -112,7 +111,7 @@ class SDM_eMember_Integration {
 			$query = parse_url( $href, PHP_URL_QUERY );
 			parse_str( $query, $query_args );
 			if ( isset( $query_args['download_id'] ) ) {
-				$download_id  = $query_args['download_id'];
+				$download_id = $query_args['download_id'];
 				if ( ! $this->is_download_permitted( $download_id ) ) {
 					$link->removeAttribute( 'href' );
 					$link->setAttribute( 'class', 'sdm_download disabled' );
